8ac07d653d63561841bf241db9d7508c9235aaf4
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-21 10:57:19 +0100

    setup - nose2

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+setup(
+    version='2.0.0',
+    author='Patryk Wawrzyniak',
+    packages=find_packages(),
+    python_requires='>=3.5, <4',
+    install_requires=['nose2'],
+    extras_require={
+        'test': ['coverage']
+    },
+)
