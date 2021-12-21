719635b3bfda46309fba00b319f1604f66ba710d
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-21 11:13:39 +0100

    test_check_init_val_player1

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,12 @@ from src.gra import Game
 
 class Game_test(unittest.TestCase):
 
-    def setUP(self):
+    def setUp(self):
         game = Game("Jacek", "Wojtas")
-        self.temp = game
+        self.game = game
+    
+    def test_check_init_val_player1(self):
+        self.assertEqual(self.game.player1_name, "Jacek")
 
     def tearDown(self):
-        self.temp = None
+        self.game = None
