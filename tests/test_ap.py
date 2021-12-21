52bf0b14404434d8acbd057de88528de16231021
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-21 18:44:58 +0100

    assertpy setup

---
+++ tests/test_ap.py
@@ -0,0 +1,26 @@
+import unittest
+from assertpy import assert_that
+from src.gra import Game
+
+
+class Game_assertpy_test(unittest.TestCase):
+    def setUp(self):
+        game = Game("Jacek", "Wojtas")
+        self.game = game
+        self.empty_board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]]
+        self.winning_board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 2, 1, 0, 0, 0, 0],
+            [0, 2, 2, 1, 0, 0, 0],
+            [1, 2, 1, 2, 1, 0, 0]]
+
+    def tearDown(self):
+        self.game = None
