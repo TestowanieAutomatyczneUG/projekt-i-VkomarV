a22d39e835ee4128db6939dbccf90f13e284743f
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-21 11:02:41 +0100

    change player function added

--- src/gra.py
+++ src/gra.py
@@ -8,11 +8,18 @@ class Game:
         self.player2 = 2
         self.separator = 0
         self.board = [[self.separator for i in range(7)] for j in range(6)]
+        self.next = self.player1
 
     def print_board(self):
         for col in self.board:
             print(col)
         print("\n")
+
+    def change_player(self):
+        if self.next == self.player1:
+            self.next = self.player2
+        else:
+            self.next = self.player1
 
 
 gra = Game("Jacek", "Wojtas")
