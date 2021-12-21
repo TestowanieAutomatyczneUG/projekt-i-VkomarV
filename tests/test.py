25df5ea55148fd3ec941a8b6685b67bb616ba4b2
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-21 11:14:04 +0100

    test_check_init_val_player2

--- tests/test.py
+++ tests/test.py
@@ -10,5 +10,8 @@ class Game_test(unittest.TestCase):
     def test_check_init_val_player1(self):
         self.assertEqual(self.game.player1_name, "Jacek")
 
+    def test_check_init_val_player2(self):
+        self.assertEqual(self.game.player2_name, "Wojtas")
+
     def tearDown(self):
         self.game = None
