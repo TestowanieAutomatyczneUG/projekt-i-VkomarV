9316bd4ba68e21e19c7b723e50da7f8e17bcb9be
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-21 18:55:00 +0100

    test_iterable

--- tests/test_ap.py
+++ tests/test_ap.py
@@ -22,8 +22,11 @@ class Game_assertpy_test(unittest.TestCase):
             [0, 2, 2, 1, 0, 0, 0],
             [1, 2, 1, 2, 1, 0, 0]]
 
-    def testInstance(self):
+    def test_instance(self):
         assert_that(self.game).is_instance_of(Game)
+
+    def test_iterable(self):
+        assert_that(self.game.move(1)).is_iterable()
 
     def tearDown(self):
         self.game = None
