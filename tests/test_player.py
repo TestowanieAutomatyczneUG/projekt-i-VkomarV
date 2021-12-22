94cd0c1fc55266156beadb443d846ebf0e62fd3b
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 22:12:49 +0100

    player test_instance

--- tests/test_player.py
+++ tests/test_player.py
@@ -7,5 +7,8 @@ class Player_test(unittest.TestCase):
     def setUp(self):
         self.player = Player("Bob", "green")
 
+    def test_instance(self):
+        assert_that(self.player, instance_of(Player))
+
     def tearDown(self):
         self.player = None
