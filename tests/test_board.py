170b0d8fa22c35b1342216ecf23cc5adbb409172
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 14:06:35 +0100

    refactor old tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -5,11 +5,106 @@ from src.board import Board
 
 class Board_test(unittest.TestCase):
     def setUp(self):
+        self.player1_color = 1
+        self.player2_color = 2
         self.board = Board(10, 10)
         self.empty_board = [[0 for i in range(10)] for j in range(10)]
 
     def test_instance(self):
         assert_that(self.board).is_instance_of(Board)
+
+    def test_check_board_not_finished(self):
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[0]).is_false()
+
+    def test_check_board_draw(self):
+        self.game.board = [
+            [1, 2, 1, 2, 1, 2, 2],
+            [1, 2, 1, 2, 1, 2, 1],
+            [2, 1, 2, 1, 2, 1, 2],
+            [2, 1, 2, 1, 2, 1, 2],
+            [1, 2, 1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1, 2, 1]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[0]).is_true()
+
+    def test_check_win_horizontal_player1(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [2, 2, 2, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0, 0]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player1_color)
+
+    def test_check_win_horizontal_player2(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [2, 2, 2, 2, 0, 0, 0],
+            [1, 1, 1, 2, 1, 1, 0]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player2_color)
+
+    def test_check_win_vertical1(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 2, 0, 0],
+            [2, 2, 0, 1, 2, 0, 0],
+            [1, 1, 0, 1, 2, 0, 0]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player1_color)
+
+    def test_check_win_vertical2(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2, 0, 0],
+            [0, 0, 0, 1, 2, 0, 0],
+            [2, 2, 0, 1, 2, 1, 0],
+            [1, 1, 0, 1, 2, 1, 0]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player2_color)
+
+    def test_check_I_slope1(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0],
+            [0, 1, 1, 2, 0, 0, 0],
+            [1, 2, 2, 2, 2, 0, 0]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player1_color)
+
+    def test_check_I_slope2(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0],
+            [2, 0, 0, 1, 1, 0, 1]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player2_color)
+
+    def test_check_II_slope1(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [2, 2, 2, 0, 1, 0, 2]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player1_color)
+
+    def test_check_II_slope2(self):
+        self.game.board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 0, 2, 0, 0, 0],
+            [1, 1, 1, 0, 2, 0, 1]]
+        assert_that(self.board.check_board(self.player1_color, self.player2_color)[1]).is_equal_to(self.player1_color)
 
     def tearDown(self):
         self.game = None
