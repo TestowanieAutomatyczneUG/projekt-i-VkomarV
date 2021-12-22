3311d4ec47f34af89551e37ae6765542cde5c72b
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 22:20:46 +0100

    player function change name added

--- src/player.py
+++ src/player.py
@@ -2,3 +2,8 @@ class Player:
     def __init__(self, name, color):
         self.name = name
         self.color = color
+
+    def change_name(self, new_name):
+        self.name = new_name
+        return self.name
+
