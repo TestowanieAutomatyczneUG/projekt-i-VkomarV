217c063dc97af5ba5f17ff13a264b32ca6b9b15b
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 14:25:04 +0100

    test_init_wrong_rows_val_string

--- src/board.py
+++ src/board.py
@@ -1,9 +1,18 @@
 class Board:
     def __init__(self, rows=6, columns=7):
-        self.separator = 0
-        self.board = [[self.separator for i in range(columns)] for j in range(rows)]
-        self.rows = rows
-        self.columns = columns
+        if type(rows) is not int:
+            raise ValueError
+        elif type(columns) is not int:
+            raise ValueError
+        elif rows < 4:
+            raise Exception("Rows too small")
+        elif columns < 4:
+            raise Exception("Columns too small")
+        else:
+            self.separator = 0
+            self.board = [[self.separator for i in range(columns)] for j in range(rows)]
+            self.rows = rows
+            self.columns = columns
 
     def print_board(self):
         for col in self.board:
