cd8bffe2c209f8420c526564b9599c4088632488
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 11:35:17 +0100

    board class added successfully

--- src/game.py
+++ src/game.py
@@ -1,22 +1,22 @@
 # rows = 6 columns = 7
 import os
 import csv
+from board import Board
 
 
-class Game:
-    def __init__(self, player1_name="player1", player2_name="player2"):
+class Game(Board):
+    def __init__(self, player1_name="player1", player2_name="player2", rows=6, columns=7):
         self.player1_name = player1_name
         self.player2_name = player2_name
         self.player1 = 1
         self.player2 = 2
-        self.separator = 0
-        self.board = [[self.separator for i in range(7)] for j in range(6)]
         self.next = self.player1
         self.moves = []
         self.winner = None
         self.looser = None
         self.game_end = False
         self.path_to_file = "data/results.csv"
+        Board.__init__(self, rows, columns)
 
     def change_player(self):
         if self.next == self.player1:
