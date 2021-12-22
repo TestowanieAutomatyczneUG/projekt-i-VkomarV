a830ad3249bf60e1641f59b2fbbfa506d5b86fb2
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 14:27:52 +0100

    test_init_wrong_rows_val_none

--- tests/test_board.py
+++ tests/test_board.py
@@ -27,6 +27,9 @@ class Board_test(unittest.TestCase):
     def test_init_wrong_rows_val_dic(self):
         self.assertRaises(ValueError, Board, {})
 
+    def test_init_wrong_rows_val_none(self):
+        self.assertRaises(ValueError, Board, None)
+
     def test_check_board_draw(self):
         self.game.board = [
             [1, 2, 1, 2, 1, 2, 2],
