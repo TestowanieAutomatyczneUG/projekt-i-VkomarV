8310ec1c0154614c3f782c589d61b4653863cb9f
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 22:15:19 +0100

    player est_color_ends_with

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,13 +5,16 @@ from src.player import Player
 
 class Player_test(unittest.TestCase):
     def setUp(self):
-        self.player = Player("Bob", "green")
+        self.player = Player("Bob", "%green%")
 
     def test_instance(self):
         assert_that(self.player, instance_of(Player))
 
-    def test_equals(self):
+    def test_correct_name(self):
         assert_that(self.player.name, equal_to("Bob"))
+
+    def test_color_ends_with(self):
+        assert_that(self.player.color, ends_with("%"))
 
     def tearDown(self):
         self.player = None
