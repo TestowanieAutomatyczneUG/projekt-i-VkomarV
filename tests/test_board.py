234e78a29930ad2c2f6e2ff1c38a1344755e95c4
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-22 14:36:41 +0100

    test_init_wrong_columns_val_none

--- tests/test_board.py
+++ tests/test_board.py
@@ -45,6 +45,9 @@ class Board_test(unittest.TestCase):
     def test_init_wrong_columns_val_dic(self):
         self.assertRaises(ValueError, Board, 10, {})
 
+    def test_init_wrong_columns_val_none(self):
+        self.assertRaises(ValueError, Board, 10, None)
+
     def test_check_board_draw(self):
         self.game.board = [
             [1, 2, 1, 2, 1, 2, 2],
