bff7c55b0b8234956f8cc72bdc6c9b4359535eec
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-23 16:42:02 +0100

    board_test_parametric test_check_board

--- tests/test_board_parametric.py
+++ tests/test_board_parametric.py
@@ -3,15 +3,26 @@ from parameterized import parameterized, parameterized_class
 from src.board import Board
 import json
 
+f = open('data/parametric_test_data.json')
+data = json.load(f)
+f.close()
+
+
+def change_to_tuple(x):
+    t = (x, )
+    return t
+
 
 class Board_test_parametric(unittest.TestCase):
     def setUp(self):
         self.player1_color = 1
         self.player2_color = 2
         self.board = Board()
-        f = open('data/parametric_test_data.json')
-        data = json.load(f)
-        f.close()
+
+    @parameterized.expand(map(change_to_tuple, data["winning_boards_patterns"]))
+    def test_check_board(self, board):
+        self.board.board = board
+        self.assertTrue(self.board.check_board(self.player1_color, self.player2_color)[0])
 
     def tearDown(self):
         self.board = None
