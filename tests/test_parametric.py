158469f161a2f53f470b8b84a650c7b301b8d56b
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-23 13:46:48 +0100

    test_move_wrong_int

--- tests/test_parametric.py
+++ tests/test_parametric.py
@@ -9,11 +9,15 @@ class Game_test_parametric(unittest.TestCase):
 
     @parameterized.expand([([], ), (11.11, ), (None, ), ("3", ), ({}, ), ((), )])
     def test_move_exceptions(self, move):
-        self.assertRaises(Exception, self.game.move, move)
+        self.assertRaises(ValueError, self.game.move, move)
 
     @parameterized.expand([(1,), (2,), (3,), (4,), (5,), (6,), (7, )])
     def test_move_correct(self, move):
         self.assertEqual(type(self.game.move(move)), type([]))
+
+    @parameterized.expand([(-1,), (0,), (8,), (9,), (-500,), (500,), (-2,)])
+    def test_move_wrong_int(self, move):
+        self.assertRaises(Exception, self.game.move, move)
 
     def tearDown(self):
         self.game = None
