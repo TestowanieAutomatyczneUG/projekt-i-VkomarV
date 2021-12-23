4ac1feceaba1708ad93e8ee48ebdca46316511bd
Azalurg <p.wawrzyniak.388@studms.ug.edu.pl>
2021-12-23 13:26:42 +0100

    parametric tests class added

--- tests/test_parametric.py
+++ tests/test_parametric.py
@@ -1,3 +1,11 @@
 import unittest
 from parameterized import parameterized, parameterized_class
 from src.game import Game
+
+
+class Game_test_parametric(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
+    def tearDown(self):
+        self.game = None
